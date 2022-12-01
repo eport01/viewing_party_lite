0429b45833e0d94de86b68dce4f83e2a17256e59
Alex M <alexm0717@hotmail.com>
2022-11-30 21:05:23 -0800

    fixed routing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,19 +7,24 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(    name: params[:name],     email: params[:email])
+    @user = User.new(user_params)
 
     if @user.valid?
       @user.save
-      redirect_to((root_path))
+      redirect_to(user_path(@user.id))
     else
-      flash[:alert] = "Try Again"
+      flash[:alert] = @user.errors.full_messages
+      redirect_to(register_path)
     end
   end
 
-  def show 
+  def show
     @user = User.find(params[:id])
   end
 
-  
+  private
+
+  def user_params
+    params.permit(:name, :email)
+  end
 end
