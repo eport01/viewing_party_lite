7ac4afa56a66b91cb06325cc60b39aafbddec274
Alex M <alexm0717@hotmail.com>
2022-12-02 09:54:16 -0800

    updated viewing party spec. all test pass

--- spec/features/viewing_party/new_spec.rb
+++ spec/features/viewing_party/new_spec.rb
@@ -13,8 +13,8 @@ RSpec.describe("New Viewing Party Page") do
     expect(page).to(have_content("Viewing Party"))
     expect(page).to(have_content("The Godfather"))
     expect(page).to(have_button("Discover Page"))
-    fill_in(:duration_of_Party,     with: "120")
-    fill_in(:day,     with: "1/1/11")
-    fill_in(:start_time,     with: "7:00")
+    fill_in(:duration,     with: "120")
+    fill_in(:date,     with: "1/1/11")
+    fill_in(:time,     with: "7:00")
   end
 end
