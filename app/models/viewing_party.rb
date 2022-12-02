f9c819de819c14c6dfe85fa4b9294762d946bcad
Emily Port <emilylport@gmail.com>
2022-12-02 15:40:12 -0700

    displaying attendees to a viewing party

--- app/models/viewing_party.rb
+++ app/models/viewing_party.rb
@@ -1,4 +1,9 @@
 class ViewingParty < ApplicationRecord
   has_many :user_viewing_parties
   has_many :users,   through: :user_viewing_parties
+
+  def attendees 
+    #get list of names of attendees for a viewing party 
+    # require 'pry'; binding.pry
+  end
 end
