c1d8d503b980f9a2eba33c7b989c37982e3362b0
Emily Port <emilylport@gmail.com>
2022-12-01 22:04:44 -0700

    create viewing party new page and create button

--- app/controllers/viewing_parties_controller.rb
+++ app/controllers/viewing_parties_controller.rb
@@ -1,3 +1,24 @@
 class ViewingPartiesController < ApplicationController
-  
+  def new 
+    @user = User.find(params[:user_id])
+
+    @result = MovieFacade.results(params[:movie_id]) 
+    # require 'pry'; binding.pry
+  end
+
+  def create 
+    @user = User.find(params[:user_id])
+    @result = MovieFacade.results(params[:movie_id])
+    @viewing_party = ViewingParty.create!(parties_params)
+    redirect_to user_path(@user)
+  end
+
+  private 
+  def parties_params 
+    params.permit(:duration, :date, :time)
+  end
+
+  def index 
+    @viewing_parties = ViewingParties.all 
+  end
 end
