d2819001eb14398153daf5ac9f39219c4aae2cd5
Emily Port <emilylport@gmail.com>
2022-12-12 12:22:20 -0700

    add email validation to user model and test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe(User) do
     it { should(have_many(:viewing_parties).through(:user_viewing_parties)) }
     it {should validate_presence_of :email}
     it {should validate_presence_of :name}
-    # it { should validate_uniqueness_of(:email)}
+    it { should validate_uniqueness_of(:email)}
     it { should validate_presence_of(:password_digest)}
     it { should have_secure_password}
   end
