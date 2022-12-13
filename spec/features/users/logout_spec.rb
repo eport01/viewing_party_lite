7ce4a194c096874d17b2e377bb58adfd9c054e95
Emily Port <emilylport@gmail.com>
2022-12-13 12:08:26 -0700

    after a user logs in, they see a button to logout on welcome landing page

--- spec/features/users/logout_spec.rb
+++ spec/features/users/logout_spec.rb
@@ -3,13 +3,23 @@ require "rails_helper"
 
 RSpec.describe "Logging out" do
   before(:each) do
-    @steve = User.create!(name: "Steve", email: "steve.smith@gmail.com", password: 'test123', password_confirmation: 'test123')
-    @mary = User.create!(name: "Mary", email: "mary.smith@gmail.com", password: 'test432', password_confirmation: 'test432')
+    @steve = User.create!(name: "Steve", email: "steve.smith@gmail.com", password: 'test123', password_confirmation: 'test123', role: 0)
+    @mary = User.create!(name: "Mary", email: "mary.smith@gmail.com", password: 'test432', password_confirmation: 'test432', role: 0)
   end
 
-  describe 'as a logged in user when i visit landing page i dont see link for log in or create account' do 
+  describe 'after i login, when i visit landing page i dont see link for log in or create account' do 
     it 'i click log out and am taken to landing page and link is now for log in' do 
       visit root_path 
+      click_on "Log In" 
+      expect(current_path).to eq('/login')
+      fill_in("Name",         with: "Mary")
+      fill_in("Email",         with: "mary.smith@gmail.com")
+      fill_in :password, with: "test432"
+      fill_in :password_confirmation, with: "test432"
+
+      click_on "Submit" 
+      expect(current_path).to eq(user_path(@mary))
+      click_on "Home"
       expect(page).to have_link "Log out"
       click_on "Log out" 
       expect(current_path).to eq(root_path)
