43ab71dada60da6561cdb351871e4ae89400ed9c
Emily Port <emilylport@gmail.com>
2022-12-14 09:19:40 -0700

    use helper method in spec for logout

--- spec/features/users/logout_spec.rb
+++ spec/features/users/logout_spec.rb
@@ -9,16 +9,9 @@ RSpec.describe "Logging out" do
 
   describe 'after i login, when i visit landing page i dont see link for log in or create account' do 
     it 'i click log out and am taken to landing page and link is now for log in' do 
-      visit root_path 
-      click_on "Log In" 
-      expect(current_path).to eq('/login')
-      fill_in("Name",         with: "Mary")
-      fill_in("Email",         with: "mary.smith@gmail.com")
-      fill_in :password, with: "test432"
-      fill_in :password_confirmation, with: "test432"
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@mary)
 
-      click_on "Submit" 
-      expect(current_path).to eq(user_path(@mary))
+      visit root_path 
       click_on "Home"
       expect(page).to_not have_link "Log in"
       expect(page).to_not have_link "Create New User"
@@ -35,7 +28,6 @@ RSpec.describe "Logging out" do
       expect(page).to_not have_content("Existing Users")
       expect(page).to_not have_content("steve.smith@gmail.com")
       expect(page).to_not have_content("mary.smith@gmail.com")
-
     end
   end
 
